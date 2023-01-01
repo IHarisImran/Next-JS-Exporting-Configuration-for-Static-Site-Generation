d0381b0c5e63c82f35d03719e603c24964b7e641
Muhammad Haris <iharisimran@gmail.com>
2023-01-02 03:20:59 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true, // Feature for highlighting potential problems in an application.
-  swcMinify: true, // Function to transform and minify your JavaScript code for production.
+  swcMinify: true, // Function to minify your JavaScript code for production.
   trailingSlash: true, // Function to add a slash "/" after the URL pathname to minimize the potential routing problems
 };
